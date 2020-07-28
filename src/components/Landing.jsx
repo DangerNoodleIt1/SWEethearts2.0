9237b7ecf8eea12adf34b08e674678098d26675d
hobaek <smilgaru@gmail.com>
2020-07-28 16:25:33 -0700

    add Carousel slide in landingpage

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { Button, Container, Row, Col } from 'react-bootstrap';
+import { Button, Container, Row, Col, Carousel, Image } from 'react-bootstrap';
 import { withRouter } from 'react-router-dom';
 
 /*
@@ -8,37 +8,88 @@ import { withRouter } from 'react-router-dom';
  * (provided by invoking withRouter on Landing component before export)
  */
 const redirectToPath = (history, path) => {
-  history.push(path);
+	history.push(path);
 };
 
 const Landing = ({ history }) => {
-  return (
-    <Container fluid className='container'>
-      <div className="mt-5">
-        <h1 className="d-flex justify-content-center">
-          {" "}
-          Welcome to Scratch Project{" "}
-        </h1>
-        <br />
-        <h2 className="mb-5 d-flex justify-content-center">
-          {" "}
-          A place where developers make their dreams come true{" "}
-        </h2>
-        <br />
-      </div>
-      <div className="mt-5 d-flex justify-content-center">
-        <Button
-          className="w-25"
-          onClick={() => redirectToPath(history, "/explore")}
-          size="lg"
-          variant="outline-primary"
-          block
-        >
-          Start Exploring
-        </Button>
-      </div>
-    </Container>
-  );
+	return (
+		<Container fluid className="container mt-4">
+			{/* <div className="mt-5">
+				<h1 className="d-flex justify-content-center">
+					{' '}
+					Welcome to Scratch Project{' '}
+				</h1>
+				<br />
+				<h2 className="mb-5 d-flex justify-content-center">
+					{' '}
+					A place where developers make their dreams come true{' '}
+				</h2>
+				<br />
+			</div> */}
+
+			<div>
+				<p>
+					<Carousel
+						style={{ width: '1110px' }}
+						// activeIndex={index}
+					>
+						<Carousel.Item>
+							<img
+								className="d-block w-100"
+								src="https://singularityhub.com/wp-content/uploads/2018/06/artificial-intelligence-confusion-719504626.jpg"
+								alt="First slide"
+							/>
+							<Carousel.Caption>
+								<h3>First slide label</h3>
+								<p>
+									Nulla vitae elit libero, a pharetra augue mollis interdum.
+								</p>
+							</Carousel.Caption>
+						</Carousel.Item>
+						<Carousel.Item>
+							<img
+								className="d-block w-100"
+								src="https://makemeaprogrammer.com/wp-content/uploads/2019/04/Are-Software-Engineers-Happy-Yes-They-Are-And-Heres-Why-1.jpg"
+								alt="Third slide"
+							/>
+
+							<Carousel.Caption>
+								<h3>Second slide label</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+							</Carousel.Caption>
+						</Carousel.Item>
+						<Carousel.Item>
+							<img
+								className="d-block w-100"
+								src="https://cdn-media-1.freecodecamp.org/images/dMoVLfSvnTDo4KKaisVJiw3l7Kcl-wKoXDNo"
+								alt="Third slide"
+							/>
+
+							<Carousel.Caption>
+								<h3>Third slide label</h3>
+								<p>
+									Praesent commodo cursus magna, vel scelerisque nisl
+									consectetur.
+								</p>
+							</Carousel.Caption>
+						</Carousel.Item>
+					</Carousel>
+				</p>
+			</div>
+
+			<div className="mt-2 d-flex justify-content-center">
+				<Button
+					className="w-100"
+					onClick={() => redirectToPath(history, '/explore')}
+					size="lg"
+					variant="outline-primary"
+					block
+				>
+					Start Exploring
+				</Button>
+			</div>
+		</Container>
+	);
 };
 
 export default withRouter(Landing);
