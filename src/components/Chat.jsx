6d88e440b361b6c50722000c4f014ff851df30c7
hobaek <smilgaru@gmail.com>
2020-07-29 14:09:36 -0700

    add profile button/ change css file

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,83 +1,76 @@
-// Create the page for the chat 
+// Create the page for the chat
 
 import React, { useState, useEffect } from 'react';
 import queryString from 'query-string'; // retrieving data from the url
-import io from 'socket.io-client/dist/socket.io'
-import {withRouter} from "react-router"
-
+import io from 'socket.io-client/dist/socket.io';
+import { withRouter } from 'react-router';
 
 // Importing components
-import InfoBar from './InfoBar'
-import Messages from './Messages'
-import Input from './Input'
-import TextContainer from './TextContainer'
+import InfoBar from './InfoBar';
+import Messages from './Messages';
+import Input from './Input';
+import TextContainer from './TextContainer';
 
-import './css/Chat.css'
+import './css/Chat.css';
 
 let socket;
 
-const Chat = ({location}) => {
+const Chat = ({ location }) => {
+	const [name, setName] = useState('');
+	const [room, setRoom] = useState('');
+	const [message, setMessage] = useState('');
+	const [messages, setMessages] = useState([]); // this stores all the messages in an array in state
 
-  const [name, setName] = useState('');
-  const [room, setRoom] = useState('');
-  const [message, setMessage] = useState('');
-  const [messages, setMessages] = useState([]);  // this stores all the messages in an array in state
+	// ! UseEffect for user joining the room
+	useEffect(() => {
+		const { name, room } = queryString.parse(location.search);
+		socket = io('localhost:3000');
+		// setting the name and the room
+		setName(name);
+		setRoom(room);
 
+		socket.emit('join', { name, room }, () => {}); // same as name: name. Sends name and room to server
 
-  // ! UseEffect for user joining the room
-  useEffect(() => {
-    const {name, room} = queryString.parse(location.search)
-    socket = io('localhost:3000');
-    // setting the name and the room
-    setName(name);
-    setRoom(room)
+		return () => {
+			socket.emit('disconnect');
+			socket.off();
+		};
+	}, ['localhost:3000', location.search]); // useEffect will trigger when values in array update
 
-    socket.emit('join', {name, room}, () => {
+	// ! UseEffect for user sending a message
+	useEffect(() => {
+		socket.on('message', (message) => {
+			setMessages([...messages, message]);
+		});
+	}, [messages]);
 
-    }); // same as name: name. Sends name and room to server
+	// function for sending message
+	const sendMessage = (event) => {
+		event.preventDefault();
+		if (message) {
+			socket.emit('sendMessage', message, () => setMessage(''));
+		}
+	};
 
-    return () => {
-      socket.emit('disconnect');
-      socket.off();
-    }
-  }, ['localhost:3000', location.search]) // useEffect will trigger when values in array update
+	console.log(message);
+	console.log(messages);
 
-
-
-  // ! UseEffect for user sending a message
-  useEffect(() => {
-    socket.on('message', (message) => {
-      setMessages([...messages, message]);
-    })
-  }, [messages])
-
-
-  // function for sending message
-  const sendMessage = (event) => {
-    event.preventDefault();
-    if (message) {
-      socket.emit('sendMessage' , message, () => setMessage(''))
-    }
-  }
-
-  console.log(message)
-  console.log(messages)
-
-
-
-
-  // ! JSX
-  return (
-<div className="outerContainer">
-      <div className="container">
-          <InfoBar room={room} />
-          <Messages messages={messages} name={name} />
-          <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
-      </div>
-      {/* <TextContainer/> */}
-      {/* <TextContainer users={users}/> */}
-    </div>
-  );
+	// ! JSX
+	return (
+		<div className="outerContainer">
+			<div className="container2">
+				<InfoBar room={room} />
+				<Messages messages={messages} name={name} />
+				<Input
+					message={message}
+					setMessage={setMessage}
+					sendMessage={sendMessage}
+				/>
+			</div>
+			{/* <TextContainer/> */}
+			{/* <TextContainer users={users}/> */}
+		</div>
+	);
 };
 
 export default withRouter(Chat);
